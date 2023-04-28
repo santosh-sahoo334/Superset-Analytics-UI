8b38d18469e6e2ff3a989cff6db3adca02ee50c8
santosh.sahoo <santosh.sahoo@teksecur.com>
2023-04-28 12:18:48 +0530

    Updated DeckGL Legend, Loading GIF

--- superset-frontend/plugins/legacy-preset-chart-deckgl/src/components/Legend.jsx
+++ superset-frontend/plugins/legacy-preset-chart-deckgl/src/components/Legend.jsx
@@ -112,8 +112,8 @@ export default class Legend extends React.PureComponent {
     const categories = Object.entries(this.props.categories).map(([k, v]) => {
       const style = { color: `rgba(${v.color.join(', ')})` };
       const icon = v.enabled ? '\u25FC' : '\u25FB';
-      const metric = v.metricsvalue ? v.metricsvalue.toFixed(2) : '';
-      const totalcount = v.totalcount ? v.totalcount : '';
+      const metric = v.metricsvalue ? v.metricsvalue.toFixed(2) + '%' : '';
+      const totalcount = v.totalcount ? '(' + v.totalcount + ')' : '';
 
       return (
         <li key={k}>
@@ -123,7 +123,7 @@ export default class Legend extends React.PureComponent {
             onDoubleClick={() => this.props.showSingleCategory(k)}
           >
             <span style={style}>{icon}</span> {this.formatCategoryLabel(k)}{' '}
-            {metric} % ( {totalcount} )
+            {metric} {totalcount} 
           </a>
         </li>
       );
