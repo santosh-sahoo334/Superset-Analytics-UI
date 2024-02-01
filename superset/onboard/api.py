3a470d90144d1166d47f56071cf13c905ae5aeda
Kaushik Bhave <kaushik.bhave@teksecur.com>
2024-02-01 17:51:50 +0530

    Updated different keys for the onboard app

--- superset/onboard/api.py
+++ superset/onboard/api.py
@@ -1,19 +1,18 @@
 from flask import Response, request
 from flask_appbuilder import expose
 from flask_appbuilder.api import BaseApi
-from flask_appbuilder.const import API_SECURITY_VERSION
 
 from superset.onboard.schemas import OnboardSchema
 
 
 class OnboardApi(BaseApi):
     resource_name = 'onboard-api'
-    route_base = '/onboard'
+    route_base = '/api/v1/onboard'
     openapi_spec_tag = "Onboard App"
-    version = API_SECURITY_VERSION
 
     add_model_schema = OnboardSchema()
-    add_columns = ["csight_key"]
+    add_columns = ["csight_key", "dora_key", "valuestream_key",
+                   "operationalmetrics_key"]
 
     @expose('/app', methods=['POST'])
     def post(self) -> Response:
@@ -52,9 +51,19 @@ class OnboardApi(BaseApi):
 
             item = self.add_model_schema.load(request.json)
             if 'csight_key' in item:
-                if item['csight_key'] == "test":
+                if item['csight_key'] == "84a20ad6-cffd-4f26-b307-6c6c867c5c0f":
                     return self.response(200, message="Csight enabled for onboard")
-            return self.response(500, message="Does not exist",
-                                 data={"msg": "App does not exist"})
+            if 'dora_key' in item:
+                if item['dora_key'] == "d5383723-36fc-43a6-bf7b-f2ceeedb319d":
+                    return self.response(200, message="Dora enabled for onboard")
+            if 'valuestream_key' in item:
+                if item['valuestream_key'] == "95f6b0ac-0386-4a8d-88d1-af11f71a8a2d":
+                    return self.response(200, message="ValueStream enabled for onboard")
+            if 'operationalmetrics_key' in item:
+                if item[
+                    'operationalmetrics_key'] == "6bb84d33-d1bd-4a5c-ad8a-907a7dd7fcfe":
+                    return self.response(200,
+                                         message="Operational Metrics enabled for onboard")
+            return self.response(500, message="App Does not exist")
         except Exception as e:
-            return self.response(500, message=str(e), data={})
+            return self.response(500, message=str(e))
--- superset/onboard/schemas.py
+++ superset/onboard/schemas.py
@@ -3,4 +3,7 @@ from marshmallow import Schema, fields
 
 class OnboardSchema(Schema):
     csight_key = fields.String(required=False)
+    dora_key = fields.String(required=False)
+    valuestream_key = fields.String(required=False)
+    operationalmetrics_key = fields.String(required=False)
 
