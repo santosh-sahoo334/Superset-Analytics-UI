409a15fdac5ebb8c9cee002c7865864734ec6e57
santosh.sahoo <santosh.sahoo@teksecur.com>
2024-02-08 11:33:44 +0530

    Remove About Section

--- superset-frontend/src/features/home/RightMenu.tsx
+++ superset-frontend/src/features/home/RightMenu.tsx
@@ -487,33 +487,6 @@ const RightMenu = ({
               </Menu.Item>
             </Menu.ItemGroup>,
           ]}
-          {(navbarRight.version_string || navbarRight.version_sha) && [
-            <Menu.Divider key="version-info-divider" />,
-            <Menu.ItemGroup key="about-section" title={t('About')}>
-              <div className="about-section">
-                {navbarRight.show_watermark && (
-                  <div css={versionInfoStyles}>
-                    {t('Powered by Apache Superset')}
-                  </div>
-                )}
-                {navbarRight.version_string && (
-                  <div css={versionInfoStyles}>
-                    {t('Version')}: {navbarRight.version_string}
-                  </div>
-                )}
-                {navbarRight.version_sha && (
-                  <div css={versionInfoStyles}>
-                    {t('SHA')}: {navbarRight.version_sha}
-                  </div>
-                )}
-                {navbarRight.build_number && (
-                  <div css={versionInfoStyles}>
-                    {t('Build')}: {navbarRight.build_number}
-                  </div>
-                )}
-              </div>
-            </Menu.ItemGroup>,
-          ]}
         </SubMenu>
         {navbarRight.show_language_picker && (
           <LanguagePicker
