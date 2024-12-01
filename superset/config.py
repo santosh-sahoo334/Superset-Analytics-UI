e2d3b187dcd5b87c9006c660136568a58da2a2ba
santosh.sahoo <santosh.sahoo@teksecur.com>
2024-12-01 17:38:48 +0530

    v4.0.3 Flask Limiter Enable

--- superset/config.py
+++ superset/config.py
@@ -875,7 +875,7 @@ MAPBOX_API_KEY = os.environ.get("MAPBOX_API_KEY", "")
 SQL_MAX_ROW = 100000
 
 #Add this line to limit the maximum size (5KiB) of incoming requests [ TekSecur - 2024-11-30]
-# MAX_CONTENT_LENGTH = 10 * 1024
+MAX_CONTENT_LENGTH = 10 * 1024
 # Maximum number of rows displayed in SQL Lab UI
 # Is set to avoid out of memory/localstorage issues in browsers. Does not affect
 # exported CSVs
