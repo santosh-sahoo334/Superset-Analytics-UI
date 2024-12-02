84832716a417691615b7cba73d138715ef5de085
santosh.sahoo <santosh.sahoo@teksecur.com>
2024-12-02 12:50:39 +0530

    v4.0.0 Imaplement CORS Origin

--- superset/app.py
+++ superset/app.py
@@ -20,6 +20,7 @@ import os
 from typing import Optional
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+from flask_cors import CORS
 
 from flask import Flask
 
@@ -30,6 +31,13 @@ logger = logging.getLogger(__name__)
 
 def create_app(superset_config_module: Optional[str] = None) -> Flask:
     app = SupersetApp(__name__)
+    
+    # Fetch CORS origins from .env and convert to list
+    cors_origins = os.environ.get("CORS_ORIGINS", "").split(",")
+    logger.info(f"CORS_ORIGINS :: {cors_origins}")
+    app.config['CORS_HEADERS'] = 'Content-Type'
+    CORS(app, resources={r"/*": {"origins": cors_origins, "supports_credentials": True}})
+
     # Initialize Flask-Limiter
     limiter = Limiter(get_remote_address,
                       default_limits=["100 per minute"],
