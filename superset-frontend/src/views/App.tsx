4c407a6e04c5af7a2eaa76a4522929e44e0054f0
kishor.kumar <kishor.kumar@cloudanalogy.com>
2025-03-27 20:09:48 +0530

    fixed css and chatbot issues

--- superset-frontend/src/views/App.tsx
+++ superset-frontend/src/views/App.tsx
@@ -91,6 +91,15 @@ const LocationPathnameLogger = () => {
 
 const App = () => {
 
+  useEffect(() => {
+    if(userEmail && !adminList?.includes(userEmail)){
+      const mainMenu = document.getElementById('main-menu');
+      if (mainMenu) {
+        mainMenu.style.display = 'none';
+      }
+    }
+  }, [bootstrapData]);
+
   return (
     <Router>
       <AIBotProvider>
