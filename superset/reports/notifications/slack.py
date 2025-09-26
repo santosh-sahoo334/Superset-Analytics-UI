010c49eae53a68b07d432a41d5f21e3bffac53e2
Santosh Sahoo <santosh.sahoo@teksecur.com>
2025-09-26 15:46:18 +0530

    Remove Navigation details from email and slack alerts

--- superset/reports/notifications/slack.py
+++ superset/reports/notifications/slack.py
@@ -86,15 +86,6 @@ class SlackNotification(BaseNotification):  # pylint: disable=too-few-public-met
 
 <%(url)s|Explore in CSight>
 
-*Navigation Path*
-Here is how you can drill down the spike at a Resource ID Level.  
-1. Once Logged in, please navigate to *Cost* Tab
-2. Scroll down to the Chart - *Cost Breakdown by Resource*
-3. Click on the Resource Name, for example, AWSLambda. 
-4. Other Charts will be refreshed as per the selected Resource Name. 
-5. Scroll down to the Chart - *Cost Breakdown by Resource ID*.
-6. Sort the cost in descending order to see the Resource ID that caused the spike.
-
 %(table)s
 """,
             # name=self._content.name,
